8c06fb8c987848a1bcc03d3abca477a3f549f249
Andreas Sogaard <andreas.sogaard@gmail.com>
2018-02-23 16:21:14 +0100

    Generalise, adapt flatten converter

--- converters/flatten.py
+++ converters/flatten.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 
 # Basic import(s)
+import re
 import h5py
+import json
 import numpy as np
+from subprocess import call
 
 # ROOT import(s)
 try:
@@ -21,10 +24,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Convert ROOT files with nested structure into flat HDF5 files.")
 
+parser.add_argument('--tag', action='store', type=str, required=True,
+                    help='Tag the data category (Zee, Wev, etc.).')
 parser.add_argument('--stop', action='store', default=None, type=int,
                     help='Maximum number of events to read.')
-parser.add_argument('--split', action='store', default=1000, type=int,
-                    help='Target number of candidates per file.')
 parser.add_argument('--outdir', action='store', default="output", type=str,
                     help='Output directory.')
 parser.add_argument('--shuffle', action='store_true', default=False,
@@ -51,51 +54,44 @@ def main ():
     if not args.outdir.endswith('/'):
         args.outdir += '/'
         pass
+    
+    args.paths = sorted(args.paths)
+
+    # Base selection
+    selection = ""  # E.g.: "(p_eta > -1.5 && p_eta < 1.5)"
 
     # Read data from all files.
-    for path in args.paths:
+    for ipath, path in enumerate(args.paths):
+        print "== {}".format(path)
+
         # Read numpy array from file.
         f = ROOT.TFile(path, 'READ')
         t = f.Get('tree')
-        array = root_numpy.tree2array(t, stop=args.stop)
+        array = root_numpy.tree2array(t, stop=args.stop, selection=selection)
 
         # Convert to HDF5-ready format.
-        data_ = convert_flatten(array)
+        data = convert_flatten(array)
 
         # Filter NaN/inf rows.
         is_bad = lambda matrix: np.any(np.isnan(matrix) | np.isinf(matrix))
-        for irow in range(data_.shape[0]):
-            if True in map(is_bad, data_[irow].tolist()):
-                print "Row {}/{} is bad!".format(irow, data_.shape[0])
+        for irow in range(data.shape[0]):
+            if True in map(is_bad, data[irow].tolist()):
+                print "Row {}/{} is bad!".format(irow, data.shape[0])
                 pass
             pass
 
-        # Concatenate to existing data array
-        try:
-            data = np.concatenate((data, data_))
-        except NameError:
-            data = data_
-            pass
-        pass
-
-    # Save as HDF5
-    mkdir(args.outdir)
-    if args.split > 0:
-        idx = 0
-        while idx == 0 or idx * args.split < data.shape[0]:
-            data_split = data[idx * args.split:(idx + 1) * args.split]
-            print "Saving to {}".format(args.outdir + 'data_{:08d}.h5'.format(idx))
-            with h5py.File(args.outdir + 'data_{:08d}.h5'.format(idx), 'w') as hf:
-                hf.create_dataset('egamma',  data=data_split)
-                pass
-            idx += 1  # Increment counter
-            pass
-        pass
-    else:
-        print "Saving to {}".format(args.outdir + 'data.h5')
-        with h5py.File(args.outdir + 'data.h5', 'w') as hf:
+        # Save as HDF5
+        mkdir(args.outdir)
+        filename = 'data_{}_{:08d}.h5'.format(args.tag, ipath)
+        print "   Saving to {}".format(args.outdir + filename)
+        with h5py.File(args.outdir + filename, 'w') as hf:
             hf.create_dataset('egamma',  data=data)
             pass
+
+        # Compress
+        print "   Compressing"
+        call(['bzip2', args.outdir + filename])
+
         pass
 
     return
@@ -137,67 +133,82 @@ def convert_flatten (data):
         Flattened numpy recarray prepared for saving to HDF5 file.
     """
 
-    # Fields to read
-    scalars = ['pt', 'et', 'eta', 'phi', 'type', 'origin', 'truthPdgId', 'LHLoose', 'LHMedium', 'LHTight', 'LHValue', 'Loose', 'Medium', 'Tight']  # (num_cands,)
-    vectors = ['energy', 'gain', 'provenance', 'sampling', 'time', 'x', 'y', 'z', 'dx', 'dy', 'dz', 'eta', 'phi', 'etasize', 'phisize']  # (num_cands, num_cells)
+    # Load in variable names
+    with open('variables.json', 'r') as f:
+        var_dict = json.load(f)
+        pass
+
+    scalars = var_dict['scalars']
+    vectors = var_dict['vectors']
+
+    # Define variable name transforms
+    pattern_scalar_in  = '{}'
+    pattern_scalar_out = '{}'
+
+    pattern_vector_in  = 'pX_{}_Cells'
+    pattern_vector_out = 'cells_{}'
+
+    regex = re.compile('^p(X)?\_')   # Prettify probe variable names
 
     # Result containers
     candidates = list()
-    names      = list()
-    first = True
+    variables  = list()
 
     # Dummy field
-    v  = 'El_' + scalars[0]
-    for ievent in range(len(data[v])):
-        for icand in range(len(data[v][ievent])):
+    v = pattern_scalar_in.format(scalars[0])
+    for icand in range(len(data[v])):
+        
+        # Candidate dict
+        d = dict()
+        
+        # Append scalars
+        for key in scalars:
 
-            # Candidate dict
-            d = dict()
+            # Get variables names
+            var_in  = pattern_scalar_in .format(key)
+            var_out = pattern_scalar_out.format(key)
+            
+            # @FIXME: Hacky, but trying to improve the naming scheme a bit.
+            var_out = regex.sub('probe_', var_out)
 
-            # Append scalars
-            for key_ in scalars:
-                key  = 'El_{}'.format(key_)
-                name = 'truth_' + key_.replace('truth', '')
-
-                d[name] = data[key][ievent][icand]
-
-                if first:
-                    names.append(name)
-                    pass
+            # Store candidate data
+            d[var_out] = data[var_in][icand]
+            
+            # Add variable name
+            if var_out not in variables:
+                variables.append(var_out)
+            else:
+                assert icand > 0, "Variable {} ({}) set for first candidate.".format(var_out, var_in)
                 pass
-
-            # Append vectors
-            for key_ in vectors:
-                key  = 'El_{}_Cells'.format(key_)
-                name = 'cells_' + key_
-
-                d[name] = data[key][ievent][icand]
-
-                if first:
-                    names.append(name)
-                    pass
-                pass
-
-            # Signal field
-            name = 'signal'
-
-            d[name] = np.abs(d['truth_PdgId']) == 11  # Truth electron
-
-            if first:
-                names.append(name)
-                pass
-
-            # Append to output
-            candidates.append(d)
-
-            # Set flag(s)
-            first = False
             pass
-        pass
+        
+        # Append vectors
+        for key in vectors:
 
-    formats = [downcast_type(candidates[0][name]) for name in names]
-    dtype  = np.dtype(zip(names, formats))
-    output = np.array([tuple([d[name] for name in names]) for d in candidates], dtype=dtype)
+            # Get variables names
+            var_in  = pattern_vector_in .format(key)
+            var_out = pattern_vector_out.format(key)
+
+            # Store candidate data
+            d[var_out] = data[var_in][icand]
+            
+            # Add variable name
+            if var_out not in variables:
+                variables.append(var_out)
+            else:
+                assert icand > 0, "Variable {} ({}) set for first candidate.".format(var_out, var_in)
+                pass
+            pass
+
+        # Append candidate to output
+        candidates.append(d)
+        pass
+    pass
+
+    # Format output as numpy structured arrays.
+    formats = [downcast_type(candidates[0][var]) for var in variables]
+    dtype  = np.dtype(zip(variables, formats))
+    output = np.array([tuple([d[var] for var in variables]) for d in candidates], dtype=dtype)
 
     return output
 
