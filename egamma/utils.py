f885a6a6d64a739e9580ec157258d822cb1fc54a
Andreas Sogaard <andreas.sogaard@gmail.com>
2018-03-08 17:50:07 +0100

    Add utility methods

--- egamma/utils.py
+++ egamma/utils.py
@@ -6,6 +6,10 @@ Common utility methods for egamma project.
 
 # Basic import(s)
 import os
+import time
+import logging as log
+from subprocess import call
+
 
 def mkdir (path):
     """
@@ -26,3 +30,26 @@ def mkdir (path):
         pass
 
     return
+
+
+def cautious_remove (path):
+    """
+    ...
+    """
+    if path.startswith('/') or '*' in path:
+        log.info("cautious_remove: Refusing to remove {}".format(path))
+    else:
+        log.debug("cautious_remove: Removing.")
+        call(['rm', path])
+        pass
+    pass
+
+def unique_tmp (path):
+    """
+    Utility script to create a unique, temporary file path.
+    """
+    ID = int(time.time() * 1E+06)
+    basedir = '/'.join(path.split('/')[:-1])
+    filename = path.split('/')[-1]
+    filename = 'tmp.{:s}.{:d}'.format(filename, ID)
+    return '{}/{}'.format(basedir, filename)
