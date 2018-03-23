6e5e5de7cc7c8ae00c255e631de24f885a658220
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-03-23 10:55:36 +0000

    Improve generator design

--- egamma/generator.py
+++ egamma/generator.py
@@ -8,6 +8,7 @@ Utilities for multiprocess loading of data files.
 import os
 import time
 import h5py
+import numpy as np
 import logging as log
 import itertools
 import multiprocessing
@@ -15,319 +16,194 @@ from subprocess import call
 
 log.basicConfig(format='[%(levelname)s] %(message)s', level=log.INFO)
 
+# Keras import(s)
+import keras
 
-def cautious_remove (path):
+
+# Define SplitArray class
+class SplitArray (object):
+    """docstring for SplitArray."""
+    def __init__ (self, arrays, batch_size=1, endless=False):
+        super(SplitArray, self).__init__()
+
+        # Check(s)
+        assert isinstance(arrays, list)
+
+        # Member variable(s)
+        self.batch_size = batch_size
+        self.endless = endless
+
+        self.arrays = arrays
+        self.ranges = [(0,len(array) - 1) for array in arrays]
+        for idx in range(1, len(self.arrays)):
+            offset = self.ranges[idx - 1][1] + 1
+            self.ranges[idx] = (self.ranges[idx][0] + offset, self.ranges[idx][1] + offset)
+            pass
+        return
+
+    def __len__ (self):
+        return sum(map(len, self.arrays)) // self.batch_size
+
+    def __getitem__ (self, idx):
+
+        # Slice accessor
+        if isinstance(idx, slice):
+
+            # Check(s)
+            start = idx.start if idx.start is not None else  0
+            stop  = idx.stop  if idx.stop  is not None else -1
+            step  = idx.step  if idx.step  is not None else  1
+
+            assert step == 1, "SplitArray: Reverse iteration is not supported."
+
+            # Wrap ranges
+            if start < 0:
+                start = len(self) + start
+                pass
+            if stop < 0:
+                stop = len(self) + stop
+                pass
+
+            # Iterate through slice
+            output = list()
+            try:
+                for jdx in range(start, stop, step):
+                    output.append(SplitArray.__getitem__(self, jdx))
+                    pass
+            except StopIteration:
+                # Was out of range
+                pass
+            return output
+
+        # Single-item accessor
+        elif isinstance(idx, int):
+
+            if self.endless:
+                idx = idx % len(self)
+            elif idx >= len(self):
+                raise StopIteration
+
+            # Find start- and stop indices for, and in, containers
+            iarray_begin = map(lambda r: r[0] <=  idx      * self.batch_size <= r[1], self.ranges).index(True)
+            iarray_end   = map(lambda r: r[0] <= (idx + 1) * self.batch_size <= r[1], self.ranges).index(True)
+            ientry_begin =  idx      * self.batch_size - self.ranges[iarray_begin][0]
+            ientry_end   = (idx + 1) * self.batch_size - self.ranges[iarray_end]  [0]
+
+            # Within-array
+            if iarray_begin == iarray_end:
+                return self.arrays[iarray_begin][ientry_begin:ientry_end]
+
+            # Across-arrays
+            else:
+                return np.concatenate((self.arrays[iarray_begin][ientry_begin:],
+                                       self.arrays[iarray_end]  [:ientry_end]))
+                pass
+
+        # Not recognised
+        else:
+            raise ValueError("SplitArray.__getitem__: Received idx of unrecognised type {}.".format(type(idx)))
+
+        return None
+    pass
+
+
+# Preprocessing transforms
+def tf_ATLAS (batch):
     """
     ...
     """
-    if path.startswith('/') or '*' in path:
-        log.info("cautious_remove: Refusing to remove {}".format(path))
-    else:
-        log.debug("cautious_remove: Removing.")
-        call(['rm', path])
-        pass
-    pass
+    feats = ['LHValue']
+    feats = ['probe_{}'.format(feat) for feat in feats]
+
+    return batch[feats].astype([(feat, '<f8') for feat in feats]).view('<f8').reshape((batch.size, len(feats)))
 
 
-def unique_tmp (path):
+def tf_flat (batch):
     """
-    Utility script to create a unique, temporary file path.
+    ...
     """
-    ID = int(time.time() * 1E+06)
-    basedir = '/'.join(path.split('/')[:-1])
-    filename = path.split('/')[-1]
-    filename = 'tmp.{:s}.{:d}'.format(filename, ID)
-    return '{}/{}'.format(basedir, filename)
+    feats = ['Rhad1', 'Rhad', 'f3', 'weta2', 'Rphi', 'Reta', 'Eratio', 'f1']
+    feats = ['probe_{}'.format(feat) for feat in feats]
+
+    return batch[feats].astype([(feat, '<f8') for feat in feats]).view('<f8').reshape((batch.size, len(feats)))
 
 
-class DecompressionProcess (multiprocessing.Process):
+class Generator (keras.utils.Sequence, SplitArray):
 
-    def __init__(self, paths, queue):
-        # Base class constructor
-        multiprocessing.Process.__init__(self)
-
-        # Member variable(s)
-        self.__paths = paths
-        self.__queue = queue
-
-        # Event(s)
-        self.exit = multiprocessing.Event()
-        self.done = multiprocessing.Event()
-        pass
-
-
-    def run (self):
+    def __init__(self, paths, batch_size, endless=False, transform=lambda x:x):
         """
         ...
         """
+        # Member variable(s)
+        self.paths = paths
+        self.transform = transform
 
-        for path in itertools.cycle(self.__paths):
-            log.debug("decompression: Decompressing {} ...".format(path))
-            remove = False
+        self.files    = [h5py.File(path, 'r') for path in self.paths]
+        self.datasets = [hf['egamma'] for hf in self.files]
 
-            # Compressed with bzip2
-            if path.endswith('.bz2'):
-                path_hdf5 = path.replace('.bz2', '')
-                path_hdf5 = unique_tmp(path_hdf5)
-                assert not os.path.isfile(path_hdf5), \
-                    "Trying to unzip {} but {} alrady exists.".format(path, path_hdf5)
-                with open(path_hdf5, 'wb') as stdout:
-                    call(['bzip2', '-dkc', path], stdout=stdout)
-                    pass
-                remove = True
+        # Base class constructors
+        keras.utils.Sequence.__init__(self)
+        SplitArray.__init__(self, self.datasets, batch_size=batch_size, endless=endless)
 
-            # Compressed with gzip
-            elif path.endswith('.gz'):
-                path_hdf5 = path.replace('.gz', '')
-                path_hdf5 = unique_tmp(path_hdf5)
-                assert not os.path.isfile(path_hdf5), \
-                    "Trying to unzip {} but {} alrady exists.".format(path, path_hdf5)
-                with open(path_hdf5, 'wb') as stdout:
-                    call(['gunzip', '-c', path], stdout=stdout)
-                    pass
-                remove = True
+        print "Generator: Created with {} datasets".format(len(self.datasets))
+        return
 
-            # Uncompressed
-            elif path.endswith('.h5'):
-                path_hdf5 = path
+    def __len__ (self):
+        return SplitArray.__len__(self)
 
-            # Unsupported
-            else:
-                log.warning("Path {} not supported.".format(path))
-                continue
+    def __getitem__ (self, idx):
+        return SplitArray.__getitem__(self, idx)
 
-            # Add to queue
-            done = False
-            while not done:
-                try:
-                    self.__queue.put((path_hdf5, remove), timeout=1)
-                    done = True
-                except:
-                    # Queue is fill
-                    if self.exit.is_set():
-                        done = True
-
-                        # Clean-up
-                        if remove:
-                            cautious_remove(path_hdf5)
-                            pass
-                        pass
-                    pass
-                pass
-
-            # Manual stop condition
-            if self.exit.is_set():
-                break
+    def __del__(self):
+        for f in self.files:
+            f.close()
             pass
-
-        self.done.set()
         return
-
-
-    def stop (self):
-        self.exit.set()
-        return
-
     pass
 
 
-class StagingProcess (multiprocessing.Process):
+class MixGenerator (keras.utils.Sequence):
 
-    def __init__(self, dataset_name, batch_size, queue_in, queue_out):
-        # Base class constructor
-        multiprocessing.Process.__init__(self)
-
-        # Member variable(s)
-        self.__dataset_name = dataset_name
-        self.__batch_size   = batch_size
-        self.__queue_in     = queue_in
-        self.__queue_out    = queue_out
-
-        # Event(s)
-        self.exit = multiprocessing.Event()
-        self.done = multiprocessing.Event()
-        pass
-
-
-    def run (self):
+    def __init__(self, *generators, **kwargs):
         """
         ...
         """
-
-        while not self.exit.is_set():
-            # Get opened, decompressed HDF5 file instance
-            log.debug("staging: Waiting for input... ({})".format(self.__queue_in.qsize()))
-
-            # Get the input path, if any
-            path, remove = self.__queue_in.get()
-
-            if path == None:
-                break
-
-            quit = False
-            # Iterate through dataset
-            with h5py.File(path) as hf:
-                dataset = hf[self.__dataset_name]
-                idx = 0
-                log.debug("staging: Start processing {}".format(path))
-                while True:
-                    batch = dataset[idx * self.__batch_size:(idx + 1)*self.__batch_size]
-                    if batch.size == 0:
-                        log.debug("Batch was empty. Stopping this file ({}).".format(path))
-                        break
-                    log.debug("staging: Putting batch from {} in __queue_stage".format(path))
-
-                    # Add to queue
-                    done = False
-                    while not done:
-                        try:
-                            self.__queue_out.put(batch, timeout=1)
-                            done = True
-                        except:
-                            # Queue is fill
-                            if self.exit.is_set():
-                                done = True
-                                pass
-                            pass
-                        pass
-                    idx += 1
-
-                    # Manual stop condition
-                    if self.exit.is_set():
-                        break
-                    pass
-
-                log.debug("staging: Done processing {}".format(path, remove))
-                if remove:
-                    cautious_remove(path)
-                    pass
+        accepted_keywords = ['return_class_label', 'shuffle']
+        for kw in kwargs:
+            if kw not in accepted_keywords:
+                log.warning("MixGenerator: Keyword {} not accepted.".format(kw))
+                kwargs.pop(kw)
                 pass
             pass
+        self.generators = generators
 
-        log.warning("Exiting StagingProcess!")
-        self.done.set()
+        # Keyword arguments
+        self.return_class_label = kwargs.get('return_class_label', True)
+        self.shuffle            = kwargs.get('shuffle',            True)
         return
 
+    def __len__(self):
+        return max(map(len, self.generators))
 
-    def stop (self):
-        self.exit.set()
-        return
+    def __getitem__(self, idx):
 
-    pass
+        # Combine parts from all generators
+        parts  = [gen[idx] for gen in self.generators]
+        labels = [np.ones((part.shape[0],)) * ipart for (ipart, part) in enumerate(parts)]
+        batch = np.concatenate(parts)
+        label = np.concatenate(labels)
 
+        # Shuffle samples
+        if self.shuffle:
+            indices = np.arange(batch.shape[0])
+            np.random.shuffle(indices)
+            batch = batch[indices]
+            label = label[indices]
+            pass
 
-class Generator:
-    """
-    Data staging generator with context manager structure.
-    """
-
-    def __init__ (self, paths, dataset_name, batch_size=1024, maxsize=8, num_batches=-1):
-        """
-        Constructor.
-
-        Arguments:
-            ...
-
-        """
-
-        # Check(s)
-        # ...
-
-        # Member variable(s)
-        self.__paths = paths
-        self.__dataset_name = dataset_name
-        self.__batch_size = batch_size
-        self.__num_batches = num_batches
-
-        self.__counter = 0
-        self.__queue_stage      = multiprocessing.Queue(maxsize=maxsize)
-        self.__queue_decompress = multiprocessing.Queue(maxsize=1)
-        self.__process_decompress = DecompressionProcess(self.__paths, self.__queue_decompress)
-        self.__process_stage      = StagingProcess(self.__dataset_name, self.__batch_size, self.__queue_decompress, self.__queue_stage)
-
-        print "[INFO]  Starting generator with {} paths, batch size {} and max. queue size {}.".format(len(self.__paths), batch_size, maxsize)
-
-        # Create processes
-        #self.__process_decompress = Process(target=decompressing, args=(self.__paths, self.__queue_decompress))
-        #self.__process_stage      = Process(target=staging, args=(self.__dataset_name, self.__batch_size, self.__queue_decompress, self.__queue_stage))
-
-        # Start process threads
-        self.__process_decompress.start()
-        self.__process_stage     .start()
-        pass
-
-
-    def __iter__ (self):
-        while True:
-            yield self.next()
-
-
-    def next (self):
-        assert self.__process_stage is not None, \
-            "[ERROR] The Generator was not properly initialised."
-        batch = self.__queue_stage.get()
-        if batch is None:
-            raise StopIteration
-        if self.__num_batches > 0 and self.__counter >= self.__num_batches:
-            raise StopIteration
-        self.__counter += 1
+        # Return batch and, optionally, class labels
+        if self.return_class_label:
+            return batch, label
         return batch
-
-
-    def stop (self):
-        """
-        ...
-        """
-
-        # Stop child processes
-        self.__process_stage     .stop()
-        self.__process_decompress.stop()
-
-        log.info("Waiting for processes to finish running")
-        while not (self.__process_stage.done.is_set() and self.__process_decompress.done.is_set()):
-            time.sleep(1)
-            pass
-
-        # Retrieve remaining elements in queues
-        paths   = list()
-        batches = list()
-
-        # -- Paths
-        while self.__queue_decompress.qsize() > 0:
-            paths.append(self.__queue_decompress.get())
-            pass
-        self.__queue_decompress.close()
-
-        # -- Batches
-        while self.__queue_stage.qsize() > 0:
-            batches.append(self.__queue_stage.get())
-            pass
-        self.__queue_stage     .close()
-
-        # Clean up any remaining decompressed files.
-        if len(paths) > 0:
-            for path, remove in paths:
-                if remove:
-                    cautious_remove(path)
-                    pass
-                pass
-            pass
-
-        # Wait for processes to finish
-        self.__process_stage     .join()
-        self.__process_decompress.join()
-        return
-
-
-    def __enter__ (self):
-        return self
-
-
-    def __exit__ (self, exc_type, exc_value, exc_traceback):
-        self.stop()
-        if exc_value != None:
-            print 'Generator failed: %s' % (exc_value)
-            pass
-        return True
 
     pass
