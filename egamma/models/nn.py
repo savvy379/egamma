174cf1537fda9793c135dec2e4c20535745dc486
Andreas Søgaard <andreas.sogaard@gmail.com>
2018-03-23 11:51:19 +0000

    Generalise NN factory method; add documentation

--- egamma/models/nn.py
+++ egamma/models/nn.py
@@ -1,20 +1,31 @@
  # -*- coding: utf-8 -*-
 
- """
- Utilities for creating simple neural network models.
- """
+"""
+Utilities for creating simple neural network models.
+"""
 
- # Keras import(s)
- from keras.models import Model
- from keras.layers import Input, BatchNormalization, Dense
+# Keras import(s)
+from keras.models import Model
+from keras.layers import Input, BatchNormalization, Dense, Dropout
 
 
-def classifier_model (num_features):
+def nn_ (num_features, num_layers=3, num_nodes=16, num_outputs=1, activation_inner='relu', activation='sigmoid', batchnorm=False, dropout=None):
     """
     Factory method to create Keras classifier models.
 
     Arguments:
         num_features: Number of input features.
+        num_layers: Number of (identical) hidden layers.
+        num_nodes: Number of nodes per hidden layerself.
+        num_outputs: Number of output nodes:
+        activation_inner: Activation function to be applied to the outputs of
+            the hidden layers.
+        activation: Activation function to be applied to the output layer.
+        batchnorm: Whether to use batch-normalisation after each hidden layer.
+            Note, that batch-normalisation is always applied to the inputs, in
+            order to ensure feature standardisation by defaultself.
+        dropout: If not `None`, perform dropout regularisation after each hidden
+            layer with probability `dropout`.
 
     Returns:
         Keras model.
@@ -27,11 +38,16 @@ def classifier_model (num_features):
     l = BatchNormalization()(clf_input)
 
     # Layers(s)
-    l = Dense(16, activation='relu')(l)
-    l = Dense(16, activation='relu')(l)
-    l = Dense(16, activation='relu')(l)
+    for _ in range(num_layers):
+        l = Dense(num_nodes, activation=activation_inner)(l)
+        if batchnorm:
+            l = BatchNormalization()(l)
+            pass
+        if dropout is not None:
+            l = Dropout(dropout)(l)
+        pass
 
     # Output
-    clf_output = Dense(1, activation='sigmoid')(l)
+    clf_output = Dense(num_outputs, activation=activation)(l)
 
     return Model(inputs=clf_input, outputs=clf_output)
